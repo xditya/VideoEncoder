ef1b506fa45372306647ed0eaef7fc85db07f60a
mradulkumar-glitch <84231956+mradulkumar-glitch@users.noreply.github.com>
2021-06-23 16:26:48 +0530

    Update ffmpeg_utils.py

--- VideoEncoder/helpers/ffmpeg_utils.py
+++ VideoEncoder/helpers/ffmpeg_utils.py
@@ -50,7 +50,7 @@ def encode(filepath):
             video_opts = "-c:v copy -tag:v hvc1"
     else:
         # Transcode to h265 / hvc1
-        video_opts = "-c:v libx265 -crf 28 -tag:v hvc1 -preset fast -threads 8"
+        video_opts = "-c:v libx265 -crf 22 -tag:v hvc1 -preset medium -threads 8"
     # Get the audio channel codec
     audio_codec = get_codec(filepath, channel="a:0")
     if audio_codec == []:
